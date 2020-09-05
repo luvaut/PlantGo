91c39b7d4f662f3d44e4c5d02d083b83309583c1
wenhe <luvaut@outlook.com>
2020-09-05 18:17:54 +1000

    Initial Commit

---
+++ PlantGo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlantGo
+//
+//  Created by 赵文赫 on 5/9/20.
+//  Copyright © 2020 LUVAUT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
